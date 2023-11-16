267088d7ea6fc53fd63ee5a2ebc6bf4d25884fd9
Prothyte <alexkoveres@gmail.com>
2023-11-16 22:00:07 +0100

    New typescript react app

---
+++ pizza-time/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LandingPage from './pages/LandingPage'
+
+function App() {
+  return (
+   <React.Fragment>
+     <LandingPage />
+   </React.Fragment>
+  );
+}
+
+export default App;
---
+++ pizza-time/src/pages/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <React.Fragment>
+      <div className='header'>
+        <h1>It's time for</h1>
+        <h1 className="header-h1">Pizza!</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default LandingPage;
